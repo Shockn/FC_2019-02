8d0be87b6457d76f5863087c333a4f8261c386dc
Shockn <57574092+Shockn@users.noreply.github.com>
2019-11-09 18:24:15 -0300

    Docstring
    Adicionada docstring com o exercício.

--- aula4Karla06.py
+++ aula4Karla06.py
@@ -1,3 +1,7 @@
+'''Fazer um programa para ler dados para um vetor. Criar um outro vetor a partir dos elementos do vetor 1, onde os elementos de 
+índices (ordem) par são multiplicados por 2 e os de ordem ímpar multiplicados por 3 e diminuídos de 1. No final imprimir os dois 
+vetores'''
+
 vet1=[]
 vet2=[]
 
