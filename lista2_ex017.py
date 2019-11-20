f1c715150dc8fea9c609564644dbe533a796982a
Yuri <yurideoliveirac@gmail.com>
2019-11-20 20:34:29 -0300

    Mudanças na impressão dos dados (adicionados os números de homens que responderam não ao lado da porcentagem)

--- lista2_ex017.py
+++ lista2_ex017.py
@@ -2,7 +2,7 @@
 mercado. Para isso, forneceu o sexo do entrevistado e sua resposta (sim ou não). Sabendo-se que foram 
 entrevistadas 2.000 pessoas, crie um algoritmo que calcule e escreva:
 a) o número de pessoas que responderam sim;
-b) o número de pessoas que responderam não;a porcentagem de pessoas do sexo masculino que responderam não.'''
+b) o número de pessoas que responderam não; a porcentagem de pessoas do sexo masculino que responderam não.'''
 
 import random
 
@@ -10,7 +10,7 @@ respostas=['SIM', 'NÃO']
 genero=['M', 'F']
 s=n=m=total=0
 
-for i in range(0, 20):
+for i in range(2000):
     sexo=genero[random.randint(0, 1)]
     resposta=respostas[random.randint(0, 1)]
     total+=1
@@ -22,14 +22,11 @@ for i in range(0, 20):
             n+=1
         else:
             n+=1
-    print(sexo, resposta)
-
-print(m)
 
 mN=(m*100)/n
 
 print('\n{:1d} pessoas responderam SIM'.format(s))
 print('{:1d} pessoas responderam NÃO'.format(n))
-print('{:1.2f}% foi a porcentagem de homens que responderam NÃO'.format(mN))
+print('{:1.2f}% dos homens responderam NÃO ({:1d} homens)'.format(mN, m))
 
 
